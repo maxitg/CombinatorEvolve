6cb64d62efe2bf0793b1fcd65b9e0650547d5d32
Max Piskunov <maxitg@icloud.com>
2020-11-20 21:42:45 -0600

    Bump version to 0.0.3.

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,6 @@
 Paclet[
   Name -> "CombinatorEvolve",
-  Version -> "0.0.2",
+  Version -> "0.0.3",
   MathematicaVersion -> "12.1+",
   Description -> "CombinatorEvolve computes SK combinator leaf counts.",
   Creator -> "Wolfram Research",
