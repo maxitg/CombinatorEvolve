64382426a96ab396601b6da576375b31cc9dbfa3
Max Piskunov <maxitg@icloud.com>
2021-02-19 13:15:39 -0600

    Search for DLLs in an explicit path.

--- cmake/Dependencies/FindGMP.cmake
+++ cmake/Dependencies/FindGMP.cmake
@@ -2,7 +2,7 @@ find_package(PkgConfig REQUIRED)
 pkg_check_modules(PKG gmp gmpxx)
 
 find_path(GMP_INCLUDE_DIR gmp.h HINTS ${PKG_gmp_INCLUDEDIR})
-find_library(GMP_LIB NAMES gmp gmp.dll HINTS ${PKG_gmp_LIBDIR})
+find_library(GMP_LIB NAMES gmp HINTS ${PKG_gmp_LIBDIR} C:/Users/circleci/msys64/mingw64/bin)
 
 message(STATUS "GMP_INCLUDE_DIR: ${GMP_INCLUDE_DIR}")
 message(STATUS "GMP_LIB: ${GMP_LIB}")
@@ -10,7 +10,7 @@ message(STATUS "PKG_gmp_INCLUDEDIR: ${PKG_gmp_INCLUDEDIR}")
 message(STATUS "PKG_gmp_LIBDIR: ${PKG_gmp_LIBDIR}")
 
 find_path(GMPXX_INCLUDE_DIR gmpxx.h HINTS ${PKG_gmpxx_INCLUDEDIR})
-find_library(GMPXX_LIB NAMES gmpxx gmpxx.dll HINTS ${PKG_gmpxx_LIBDIR})
+find_library(GMPXX_LIB NAMES gmpxx HINTS ${PKG_gmpxx_LIBDIR} C:/Users/circleci/msys64/mingw64/bin)
 
 if(GMP_FOUND)
   if(NOT TARGET GMP)
