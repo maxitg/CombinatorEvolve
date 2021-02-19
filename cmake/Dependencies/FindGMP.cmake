08f4af1e4ced3729eb9594f44523ccf4dd40a27b
Max Piskunov <maxitg@icloud.com>
2021-02-19 13:05:59 -0600

    Add more status messages.

--- cmake/Dependencies/FindGMP.cmake
+++ cmake/Dependencies/FindGMP.cmake
@@ -3,7 +3,11 @@ pkg_check_modules(PKG gmp gmpxx)
 
 find_path(GMP_INCLUDE_DIR gmp.h HINTS ${PKG_gmp_INCLUDEDIR})
 find_library(GMP_LIB gmp HINTS ${PKG_gmp_LIBDIR})
+
+message(STATUS "GMP_INCLUDE_DIR: ${GMP_INCLUDE_DIR}")
 message(STATUS "GMP_LIB: ${GMP_LIB}")
+message(STATUS "PKG_gmp_INCLUDEDIR: ${PKG_gmp_INCLUDEDIR}")
+message(STATUS "PKG_gmp_LIBDIR: ${PKG_gmp_LIBDIR}")
 
 find_path(GMPXX_INCLUDE_DIR gmpxx.h HINTS ${PKG_gmpxx_INCLUDEDIR})
 find_library(GMPXX_LIB gmpxx HINTS ${PKG_gmpxx_LIBDIR})
