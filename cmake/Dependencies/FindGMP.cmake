9e7f9d62d48d8a4dd9e4cdad79ebdb961266a065
Max Piskunov <maxitg@icloud.com>
2021-02-19 16:45:42 -0600

    Use STREQUAL.

--- cmake/Dependencies/FindGMP.cmake
+++ cmake/Dependencies/FindGMP.cmake
@@ -4,15 +4,16 @@ pkg_check_modules(PKG gmp gmpxx)
 find_path(GMP_INCLUDE_DIR gmp.h HINTS ${PKG_gmp_INCLUDEDIR})
 find_library(GMP_LIB gmp HINTS ${PKG_gmp_LIBDIR})
 
-if(GMP_LIB EQUAL "GMP_LIB-NOTFOUND")
-  message(STATUS "not found")
+if("${GMP_LIB}" STREQUAL "GMP_LIB-NOTFOUND")
+  message(STATUS "not found 1")
   find_file(GMP_LIB libgmp.dll.a HINTS ${PKG_gmp_LIBDIR})
 endif()
 
 find_path(GMPXX_INCLUDE_DIR gmpxx.h HINTS ${PKG_gmpxx_INCLUDEDIR})
 find_library(GMPXX_LIB gmpxx HINTS ${PKG_gmpxx_LIBDIR})
 
-if(GMPXX_LIB EQUAL "GMPXX_LIB-NOTFOUND")
+if("${GMPXX_LIB}" STREQUAL "GMPXX_LIB-NOTFOUND")
+  message(STATUS "not found 2")
   find_file(GMPXX_LIB libgmpxx.dll.a HINTS ${PKG_gmpxx_LIBDIR})
 endif()
 
