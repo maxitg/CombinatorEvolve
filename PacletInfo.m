cb12e9b03c44ec14509aae2a0401462344d523ad
Max Piskunov <maxitg@icloud.com>
2021-02-19 17:24:17 -0600

    Fix install script.

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,6 +1,6 @@
 Paclet[
   Name -> "CombinatorEvolve",
-  Version -> "0.0.5",
+  Version -> "0.1",
   MathematicaVersion -> "12.1+",
   Description -> "CombinatorEvolve computes SK combinator leaf counts.",
   Creator -> "Wolfram Research",
