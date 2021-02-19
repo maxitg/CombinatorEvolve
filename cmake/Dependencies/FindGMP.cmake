7209b9c34e3e873ec1006a74d8cfcc9de0b28ef8
Max Piskunov <maxitg@icloud.com>
2021-02-19 16:51:06 -0600

    Clean up GMP messages.

--- cmake/Dependencies/FindGMP.cmake
+++ cmake/Dependencies/FindGMP.cmake
@@ -5,7 +5,6 @@ find_path(GMP_INCLUDE_DIR gmp.h HINTS ${PKG_gmp_INCLUDEDIR})
 find_library(GMP_LIB gmp HINTS ${PKG_gmp_LIBDIR})
 
 if("${GMP_LIB}" STREQUAL "GMP_LIB-NOTFOUND")
-  message(STATUS "not found 1")
   find_file(GMP_LIB libgmp.dll.a HINTS ${PKG_gmp_LIBDIR})
 endif()
 
@@ -13,14 +12,13 @@ find_path(GMPXX_INCLUDE_DIR gmpxx.h HINTS ${PKG_gmpxx_INCLUDEDIR})
 find_library(GMPXX_LIB gmpxx HINTS ${PKG_gmpxx_LIBDIR})
 
 if("${GMPXX_LIB}" STREQUAL "GMPXX_LIB-NOTFOUND")
-  message(STATUS "not found 2")
   find_file(GMPXX_LIB libgmpxx.dll.a HINTS ${PKG_gmpxx_LIBDIR})
 endif()
 
 message(STATUS "GMP_INCLUDE_DIR: ${GMP_INCLUDE_DIR}")
 message(STATUS "GMP_LIB: ${GMP_LIB}")
-message(STATUS "PKG_gmp_INCLUDEDIR: ${PKG_gmp_INCLUDEDIR}")
-message(STATUS "PKG_gmp_LIBDIR: ${PKG_gmp_LIBDIR}")
+message(STATUS "GMPXX_INCLUDE_DIR: ${GMPXX_INCLUDE_DIR}")
+message(STATUS "GMPXX_LIB: ${GMPXX_LIB}")
 
 if(GMP_FOUND)
   if(NOT TARGET GMP)
